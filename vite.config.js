2c982e03e6304b18a8fe86d14ac1e45a036aafb8
mertcanmerkit <mertcanmerkit@gmail.com>
2025-05-19 14:55:03 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite';
-import vue from "@vitejs/plugin-vue";
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
-        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
     ],
-});
+})
