f6707be56fa9b9d5647050b486de09340ac2ea1b
mertcanmerkit <mertcanmerkit@gmail.com>
2025-05-19 15:25:11 +0200

    build

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {useToast} from 'vue-toast-notification';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: import.meta.env.VITE_API_BASE_URL || '/',
     withCredentials: true,
     withXSRFToken: true,
 });
